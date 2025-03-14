30a96ab5a1f63c37b77a7c2a2fc9f355922c800d
Svvida <ksvvida@gmail.com>
2025-03-14 16:15:22 +0100

    QuickFix for repository method that retrieve active quests

--- Infrastructure/Repositories/Quests/QuestMetadataRepository.cs
+++ Infrastructure/Repositories/Quests/QuestMetadataRepository.cs
@@ -25,16 +25,28 @@ namespace Infrastructure.Repositories.Quests
             DateTime today = DateTime.UtcNow.Date;
 
             var baseQuery = _context.QuestsMetadata
-                .Where(q => q.AccountId == accountId)
-                .AsQueryable();
+                 .Where(q => q.AccountId == accountId &&
+                     (
+                         (q.QuestType == QuestTypeEnum.OneTime && q.OneTimeQuest != null &&
+                             (!q.OneTimeQuest.StartDate.HasValue || q.OneTimeQuest.StartDate.Value.Date <= today) &&
+                             (!q.OneTimeQuest.EndDate.HasValue || q.OneTimeQuest.EndDate.Value.Date >= today))
 
-            baseQuery = baseQuery.Where(q =>
-                (q.QuestType == QuestTypeEnum.OneTime && q.OneTimeQuest != null && IsQuestActive(q.OneTimeQuest.StartDate, q.OneTimeQuest.EndDate, today)) ||
-                (q.QuestType == QuestTypeEnum.Daily && q.DailyQuest != null && IsQuestActive(q.DailyQuest.StartDate, q.DailyQuest.EndDate, today)) ||
-                (q.QuestType == QuestTypeEnum.Weekly && q.WeeklyQuest != null && IsQuestActive(q.WeeklyQuest.StartDate, q.WeeklyQuest.EndDate, today)) ||
-                (q.QuestType == QuestTypeEnum.Monthly && q.MonthlyQuest != null && IsQuestActive(q.MonthlyQuest.StartDate, q.MonthlyQuest.EndDate, today)) ||
-                (q.QuestType == QuestTypeEnum.Seasonal && q.SeasonalQuest != null && IsQuestActive(q.SeasonalQuest.StartDate, q.SeasonalQuest.EndDate, today))
-            );
+                         || (q.QuestType == QuestTypeEnum.Daily && q.DailyQuest != null &&
+                             (!q.DailyQuest.StartDate.HasValue || q.DailyQuest.StartDate.Value.Date <= today) &&
+                             (!q.DailyQuest.EndDate.HasValue || q.DailyQuest.EndDate.Value.Date >= today))
+
+                         || (q.QuestType == QuestTypeEnum.Weekly && q.WeeklyQuest != null &&
+                             (!q.WeeklyQuest.StartDate.HasValue || q.WeeklyQuest.StartDate.Value.Date <= today) &&
+                             (!q.WeeklyQuest.EndDate.HasValue || q.WeeklyQuest.EndDate.Value.Date >= today))
+
+                         || (q.QuestType == QuestTypeEnum.Monthly && q.MonthlyQuest != null &&
+                             (!q.MonthlyQuest.StartDate.HasValue || q.MonthlyQuest.StartDate.Value.Date <= today) &&
+                             (!q.MonthlyQuest.EndDate.HasValue || q.MonthlyQuest.EndDate.Value.Date >= today))
+
+                         || (q.QuestType == QuestTypeEnum.Seasonal && q.SeasonalQuest != null &&
+                             (!q.SeasonalQuest.StartDate.HasValue || q.SeasonalQuest.StartDate.Value.Date <= today) &&
+                             (!q.SeasonalQuest.EndDate.HasValue || q.SeasonalQuest.EndDate.Value.Date >= today))
+                     )).AsQueryable();
 
             baseQuery = baseQuery
                 .Select(q => new QuestMetadata
