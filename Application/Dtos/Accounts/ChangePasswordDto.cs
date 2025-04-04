cdc727e6c627fd55f02accc617594941b51da96b
Svvida <ksvvida@gmail.com>
2025-04-04 19:12:46 +0200

    Updated change password request body and validator

--- Application/Dtos/Accounts/ChangePasswordDto.cs
+++ Application/Dtos/Accounts/ChangePasswordDto.cs
@@ -3,6 +3,8 @@
     public class ChangePasswordDto
     {
         public string OldPassword { get; set; } = null!;
+        public string ConfirmOldPassword { get; set; } = null!;
         public string NewPassword { get; set; } = null!;
+        public string ConfirmNewPassword { get; set; } = null!;
     }
 }
