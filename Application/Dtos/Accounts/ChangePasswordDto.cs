ab071182633d43b1343e04b0ce58e1f2acc7ea87
Svvida <ksvvida@gmail.com>
2025-04-06 19:37:37 +0200

    Removed ConfirmOldPassword from change password request body

--- Application/Dtos/Accounts/ChangePasswordDto.cs
+++ Application/Dtos/Accounts/ChangePasswordDto.cs
@@ -3,7 +3,6 @@
     public class ChangePasswordDto
     {
         public string OldPassword { get; set; } = null!;
-        public string ConfirmOldPassword { get; set; } = null!;
         public string NewPassword { get; set; } = null!;
         public string ConfirmNewPassword { get; set; } = null!;
     }
