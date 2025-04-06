110d96ca32fce81b36fc7a9a338c2a151c4ab685
Svvida <ksvvida@gmail.com>
2025-04-06 19:31:32 +0200

    Refactored leveling system for user profiles

---
+++ Application/MappingActions/SetUserLevelAction.cs
@@ -0,0 +1,33 @@
+﻿using Application.Dtos.Accounts;
+using Application.Interfaces;
+using Application.Models;
+using AutoMapper;
+using Domain.Models;
+
+namespace Application.MappingActions
+{
+    public class SetUserLevelAction : IMappingAction<Account, GetAccountDto>
+    {
+        private readonly ILevelingService _levelingService;
+        public SetUserLevelAction(ILevelingService levelingService)
+        {
+            _levelingService = levelingService;
+        }
+        public void Process(Account source, GetAccountDto destination, ResolutionContext context)
+        {
+            if (source.Profile != null)
+            {
+                LevelInfo levelInfo = _levelingService.CalculateLevelInfo(source.Profile.TotalXp);
+
+                destination.Level = levelInfo.CurrentLevel;
+                destination.UserXp = source.Profile.TotalXp;
+                destination.IsMaxLevel = levelInfo.IsMaxLevel;
+
+                if (levelInfo.IsMaxLevel)
+                    destination.NextLevelTotalXpRequired = levelInfo.CurrentLevelRequiredXp;
+                else
+                    destination.NextLevelTotalXpRequired = levelInfo.NextLevelRequiredXp ?? levelInfo.CurrentLevelRequiredXp; // fallback to current level if next level is null
+            }
+        }
+    }
+}
--- Application/MappingProfiles/AccountProfile.cs
+++ Application/MappingProfiles/AccountProfile.cs
@@ -1,4 +1,5 @@
 ﻿using Application.Dtos.Accounts;
+using Application.MappingActions;
 using AutoMapper;
 using Domain.Models;
 
@@ -10,16 +11,17 @@ namespace Application.MappingProfiles
         {
             // Entity -> DTO
             CreateMap<Account, GetAccountDto>()
+                .ForMember(dest => dest.JoinDate, opt => opt.MapFrom(src => src.CreatedAt))
+                // Map from the Profile entity
                 .ForMember(dest => dest.Nickname, opt => opt.MapFrom(src => src.Profile.Nickname))
                 .ForMember(dest => dest.Avatar, opt => opt.MapFrom(src => src.Profile.Avatar))
                 .ForMember(dest => dest.CompletedQuests, opt => opt.MapFrom(src => src.Profile.CompletedQuests))
                 .ForMember(dest => dest.TotalQuests, opt => opt.MapFrom(src => src.Profile.TotalQuests))
-                .ForMember(dest => dest.Level, opt => opt.MapFrom(src => src.Profile.UserLevel))
-                .ForMember(dest => dest.Xp, opt => opt.MapFrom(src => src.Profile.CurrentExperience))
-                .ForMember(dest => dest.TotalXP, opt => opt.MapFrom(src => src.Profile.TotalExperience))
                 .ForMember(dest => dest.Bio, opt => opt.MapFrom(src => src.Profile.Bio))
-                .ForMember(dest => dest.JoinDate, opt => opt.MapFrom(src => src.CreatedAt))
-                .ForMember(dest => dest.Badges, opt => opt.MapFrom(src => src.Profile.UserProfile_Badges));
+                .ForMember(dest => dest.UserXp, opt => opt.MapFrom(src => src.Profile.TotalXp))
+                .ForMember(dest => dest.Badges, opt => opt.MapFrom(src => src.Profile.UserProfile_Badges))
+                // Calculate and Map Leveling Info
+                .AfterMap<SetUserLevelAction>();
 
             // Create DTO -> Entity
             CreateMap<CreateAccountDto, Account>()
