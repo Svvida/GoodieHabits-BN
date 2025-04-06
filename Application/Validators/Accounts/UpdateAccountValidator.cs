34e591028fe9db8dd2f81d6dbd7097c0cdc45a32
Svvida <116441225+Svvida@users.noreply.github.com>
2025-04-06 19:44:45 +0200

    Changed validation on account update (#78)

--- Application/Validators/Accounts/UpdateAccountValidator.cs
+++ Application/Validators/Accounts/UpdateAccountValidator.cs
@@ -9,7 +9,7 @@ namespace Application.Validators.Accounts
         public UpdateAccountValidator()
         {
             RuleFor(x => x.Login)
-                .Length(3, 15).WithMessage("{PropertyName} must be between {MinLength} and {MaxLength} characters.")
+                .Length(3, 16).WithMessage("{PropertyName} must be between {MinLength} and {MaxLength} characters.")
                 .Matches("^[a-zA-Z0-9.!_-]*$").WithMessage("{PropertyName} must contain only letters, numbers, and the following special characters: . ! _ -")
                 .When(x => !string.IsNullOrEmpty(x.Login));
 
@@ -19,11 +19,11 @@ namespace Application.Validators.Accounts
                 .Must(input => Checkers.IsEmail(input)).WithMessage("{PropertyName} must be a valid email address");
 
             RuleFor(x => x.Nickname)
-                .Length(3, 15).WithMessage("{PropertyName} must be between {MinLength} and {MaxLength} characters.")
+                .Length(3, 16).WithMessage("{PropertyName} must be between {MinLength} and {MaxLength} characters.")
                 .When(x => !string.IsNullOrEmpty(x.Nickname));
 
             RuleFor(x => x.Bio)
-                .MaximumLength(150).WithMessage("{PropertyName} cannot exceed {MaxLength} characters")
+                .MaximumLength(30).WithMessage("{PropertyName} cannot exceed {MaxLength} characters")
                 .Must(input => Checkers.IsSafeHtml(input)).WithMessage("{PropertyName} contains unsafe HTML")
                 .When(x => !string.IsNullOrEmpty(x.Bio));
         }
