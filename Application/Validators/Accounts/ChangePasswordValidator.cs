574a88b9d5a5ecae8090932c76db46e817b9865b
Svvida <116441225+Svvida@users.noreply.github.com>
2025-04-06 19:38:31 +0200

    Removed ConfirmOldPassword from change password request body (#76)

--- Application/Validators/Accounts/ChangePasswordValidator.cs
+++ Application/Validators/Accounts/ChangePasswordValidator.cs
@@ -14,15 +14,6 @@ namespace Application.Validators.Accounts
                 .Matches("^[a-zA-Z0-9_#@!-]*$")
                 .WithMessage("{PropertyName} must contain only letters, numbers, and the following special characters: _ @ # -");
 
-            RuleFor(x => x.ConfirmOldPassword)
-                .NotEmpty().WithMessage("{PropertyName} is required")
-                .MinimumLength(6).WithMessage("{PropertyName} must be at least {MinLength} characters long.")
-                .MaximumLength(50).WithMessage("{PropertyName} must not exceed {MaxLength} characters.")
-                .Matches("^[a-zA-Z0-9_#@!-]*$")
-                .WithMessage("{PropertyName} must contain only letters, numbers, and the following special characters: _ @ # -")
-                .Equal(x => x.OldPassword)
-                .WithMessage("The confirmation password does not match your old password.");
-
             RuleFor(x => x.NewPassword)
                 .NotEmpty().WithMessage("{PropertyName} is required")
                 .MinimumLength(6).WithMessage("{PropertyName} must be at least {MinLength} characters long.")
