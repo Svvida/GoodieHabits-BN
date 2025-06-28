06c30898f7f072e2c7482b4f53789106a36bdbe5
Svvida <ksvvida@gmail.com>
2025-06-28 17:35:06 +0200

    New endpoint to return extended user stats

--- Application/Interfaces/IStatsService.cs
+++ Application/Interfaces/IStatsService.cs
@@ -1,9 +1,11 @@
-﻿using Application.Dtos.UserProfileStats;
+﻿using Application.Dtos.Stats;
+using Application.Dtos.UserProfileStats;
 
 namespace Application.Interfaces
 {
     public interface IStatsService
     {
         Task<GetUserProfileStatsDto> GetUserProfileStatsAsync(int accountId, CancellationToken cancellationToken = default);
+        Task<GetUserExtendedStatsDto> GetUserExtendedStatsAsync(int accountId, CancellationToken cancellationToken = default);
     }
 }
---
+++ Application/MappingProfiles/ExtendedStatsProfile.cs
@@ -0,0 +1,32 @@
+﻿using Application.Dtos.Stats;
+using Application.Dtos.UserProfileStats;
+using Application.MappingActions;
+using AutoMapper;
+using Domain.Models;
+
+namespace Application.MappingProfiles
+{
+    public class ExtendedStatsProfile : Profile
+    {
+        public ExtendedStatsProfile()
+        {
+            CreateMap<UserProfile, GetUserExtendedStatsDto>()
+                .ForMember(dest => dest.QuestStats, opt => opt.MapFrom(src => src))
+                .ForMember(dest => dest.GoalStats, opt => opt.MapFrom(src => src))
+                .ForMember(dest => dest.XpStats, opt => opt.MapFrom(src => src));
+
+            CreateMap<UserProfile, QuestExtendedStatsDto>()
+                .ForMember(dest => dest.CurrentTotal, opt => opt.MapFrom(src => src.ExistingQuests))
+                .ForMember(dest => dest.CurrentEverCompleted, opt => opt.MapFrom(src => src.EverCompletedExistingQuests))
+                .ForMember(dest => dest.CurrentCompleted, opt => opt.MapFrom(src => src.CurrentlyCompletedExistingQuests))
+                .ForMember(dest => dest.TotalCreated, opt => opt.MapFrom(src => src.TotalQuests))
+                .ForMember(dest => dest.TotalCompleted, opt => opt.MapFrom(src => src.CompletedQuests));
+
+            CreateMap<UserProfile, GoalExtendedStatsDto>()
+                .AfterMap<SetGoalExtendedStatsAction>();
+
+            CreateMap<UserProfile, XpProgressDto>()
+                .AfterMap<SetUserLevelAction>();
+        }
+    }
+}
