0b7ac1150950b9dbd2793daadd81b3f87ba51d0a
Svvida <ksvvida@gmail.com>
2025-07-02 21:22:57 +0200

    Implemented random nickname generator and exposed endpoint to retrieve random nickname

---
+++ Api/Controllers/NicknameController.cs
@@ -0,0 +1,28 @@
+﻿using Application.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    [Authorize]
+    public class NicknameController : ControllerBase
+    {
+        private readonly INicknameGeneratorService _nicknameGeneratorService;
+
+        public NicknameController(INicknameGeneratorService nicknameGeneratorService)
+        {
+            _nicknameGeneratorService = nicknameGeneratorService;
+        }
+
+        [HttpGet("random")]
+        public async Task<ActionResult<string>> GenerateUniqueNickname(CancellationToken cancellationToken = default)
+        {
+
+            var randomNickname = await _nicknameGeneratorService.GenerateUniqueNicknameAsync(cancellationToken);
+
+            return Ok(randomNickname);
+        }
+    }
+}
---
+++ Infrastructure/Migrations/20250702191400_RequiredNicknameMaxLength30.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class RequiredNicknameMaxLength30 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_UserProfiles_Nickname",
+                table: "UserProfiles");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Nickname",
+                table: "UserProfiles",
+                type: "nvarchar(30)",
+                maxLength: 30,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(16)",
+                oldMaxLength: 16,
+                oldNullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserProfiles_Nickname",
+                table: "UserProfiles",
+                column: "Nickname",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_UserProfiles_Nickname",
+                table: "UserProfiles");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Nickname",
+                table: "UserProfiles",
+                type: "nvarchar(16)",
+                maxLength: 16,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(30)",
+                oldMaxLength: 30);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserProfiles_Nickname",
+                table: "UserProfiles",
+                column: "Nickname",
+                unique: true,
+                filter: "[Nickname] IS NOT NULL");
+        }
+    }
+}
