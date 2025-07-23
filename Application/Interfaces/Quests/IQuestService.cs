af84a8147498651e8487a4c65eb5efe07f985a29
Svvida <ksvvida@gmail.com>
2025-07-23 20:46:34 +0200

    Created get quest by id query handler

--- Application/Interfaces/Quests/IQuestService.cs
+++ Application/Interfaces/Quests/IQuestService.cs
@@ -5,7 +5,6 @@ namespace Application.Interfaces.Quests
 {
     public interface IQuestService
     {
-        Task<BaseGetQuestDto?> GetUserQuestByIdAsync(int questId, QuestTypeEnum questType, CancellationToken cancellationToken = default);
         Task<IEnumerable<BaseGetQuestDto>> GetAllUserQuestsByTypeAsync(int accountId, QuestTypeEnum questType, CancellationToken cancellationToken = default);
         Task<IEnumerable<BaseGetQuestDto>> GetActiveQuestsAsync(int accountId, CancellationToken cancellationToken = default);
         Task<BaseGetQuestDto> CreateUserQuestAsync(BaseCreateQuestDto createDto, QuestTypeEnum questType, CancellationToken cancellationToken = default);
---
+++ Application/Quests/Queries/GetQuestByIdQueryHandler.cs
@@ -0,0 +1,22 @@
+﻿using Application.Dtos.Quests;
+using Application.Interfaces.Quests;
+using Domain.Interfaces;
+using MediatR;
+
+namespace Application.Quests.Queries
+{
+    public class GetQuestByIdQueryHandler(IUnitOfWork unitOfWork, IQuestMappingService questMappingService) : IRequestHandler<GetQuestByIdQuery, BaseGetQuestDto?>
+    {
+        private readonly IUnitOfWork _unitOfWork = unitOfWork;
+        private readonly IQuestMappingService _questMappingService = questMappingService;
+
+        public async Task<BaseGetQuestDto?> Handle(GetQuestByIdQuery request, CancellationToken cancellationToken = default)
+        {
+            var quest = await _unitOfWork.Quests.GetQuestByIdAsync(request.QuestId, request.QuestType, true, cancellationToken);
+            if (quest is null)
+                return null;
+
+            return _questMappingService.MapToDto(quest);
+        }
+    }
+}
