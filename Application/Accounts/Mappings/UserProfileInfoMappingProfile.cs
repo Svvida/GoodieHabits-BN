8508bae9b86f94b5ec313e5ce3d550bd7d7b7a10
Svvida <ksvvida@gmail.com>
2025-10-18 21:57:03 +0200

    Badges are now returned on User profile

--- Application/Accounts/Mappings/UserProfileInfoMappingProfile.cs
+++ Application/Accounts/Mappings/UserProfileInfoMappingProfile.cs
@@ -10,11 +10,14 @@ namespace Application.Accounts.Mappings
         public void Register(TypeAdapterConfig config)
         {
             config.NewConfig<UserProfile, UserProfileInfoDto>()
-                .Map(dest => dest.Badges, src => src.UserProfile_Badges.Select(b => b.Adapt<BadgeDto>()))
+                .Map(dest => dest.Badges, src => src.UserProfile_Badges)
                 .Map(dest => dest.Avatar, src => MapContext.Current.GetService<IUrlBuilder>().BuildAvatarUrl(src.Avatar));
 
             config.NewConfig<UserProfile_Badge, BadgeDto>()
-                .Map(dest => dest.Text, src => src.Badge.Text);
+                .Map(dest => dest.Type, src => src.Badge.Type)
+                .Map(dest => dest.Text, src => src.Badge.Text)
+                .Map(dest => dest.ColorHex, src => src.Badge.ColorHex)
+                .Map(dest => dest.Description, src => src.Badge.Description);
         }
     }
 }
--- Application/Accounts/Queries/GetWithProfile/GetAccountWithProfileQueryHandler.cs
+++ Application/Accounts/Queries/GetWithProfile/GetAccountWithProfileQueryHandler.cs
@@ -9,7 +9,7 @@ namespace Application.Accounts.Queries.GetWithProfile
     {
         public async Task<GetAccountWithProfileResponse> Handle(GetAccountWithProfileQuery request, CancellationToken cancellationToken)
         {
-            var accountWithProfile = await unitOfWork.Accounts.GetAccountWithProfileAsync(request.AccountId, cancellationToken).ConfigureAwait(false)
+            var accountWithProfile = await unitOfWork.Accounts.GetAccountWithProfileForDisplayAsync(request.AccountId, cancellationToken).ConfigureAwait(false)
                 ?? throw new NotFoundException($"Account with ID {request.AccountId} was not found.");
             return mapper.Map<GetAccountWithProfileResponse>(accountWithProfile);
         }
