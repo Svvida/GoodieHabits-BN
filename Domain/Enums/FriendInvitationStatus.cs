4d02553994971b0bbc540ee63add35bc4a81d4b6
Svvida <ksvvida@gmail.com>
2025-11-03 21:08:34 +0100

    Completed database setup for friend feature

---
+++ Domain/Enums/FriendInvitationStatus.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.Enums
+{
+    public enum FriendInvitationStatus
+    {
+        Pending = 0,
+        Rejected = 1,
+        Cancelled = 2,
+        Accepted = 3
+    }
+}
---
+++ Infrastructure/Persistence/Configuration/UserBlockConfiguration.cs
@@ -0,0 +1,36 @@
+﻿using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Persistence.Configuration
+{
+    public class UserBlockConfiguration : IEntityTypeConfiguration<UserBlock>
+    {
+        public void Configure(EntityTypeBuilder<UserBlock> builder)
+        {
+            builder.ToTable("UserBlocks");
+
+            builder.HasKey(ub => new { ub.BlockerUserProfileId, ub.BlockedUserProfileId });
+            builder.HasIndex(ub => new { ub.BlockerUserProfileId, ub.BlockedUserProfileId }).IsUnique();
+
+            builder.Property(ub => ub.BlockerUserProfileId)
+                .IsRequired();
+            builder.Property(ub => ub.BlockedUserProfileId)
+                .IsRequired();
+
+            builder.Property(p => p.CreatedAt)
+                .IsRequired()
+                .HasDefaultValueSql("GETUTCDATE()");
+
+            builder.HasOne(ub => ub.BlockerUserProfile)
+                .WithMany(up => up.SentBlocks)
+                .HasForeignKey(ub => ub.BlockerUserProfileId)
+                .OnDelete(DeleteBehavior.NoAction);
+
+            builder.HasOne(ub => ub.BlockedUserProfile)
+                .WithMany(up => up.ReceivedBlocks)
+                .HasForeignKey(ub => ub.BlockedUserProfileId)
+                .OnDelete(DeleteBehavior.NoAction);
+        }
+    }
+}
