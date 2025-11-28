6211ce677775e9d621b5f682ed5049513fc251fd
Svvida <ksvvida@gmail.com>
2025-11-28 15:18:06 +0100

    Fixed returning user goals

--- Domain/Interfaces/Repositories/IUserGoalRepository.cs
+++ Domain/Interfaces/Repositories/IUserGoalRepository.cs
@@ -6,9 +6,9 @@ namespace Domain.Interfaces.Repositories
 {
     public interface IUserGoalRepository : IBaseRepository<UserGoal>
     {
-        Task<int> GetActiveGoalsCountByTypeAsync(int accountId, GoalTypeEnum goalType, CancellationToken cancellationToken = default);
+        Task<int> GetActiveGoalsCountByTypeAsync(int userProfileId, GoalTypeEnum goalType, CancellationToken cancellationToken = default);
         Task<UserGoal?> GetActiveGoalByQuestIdAsync(int questId, CancellationToken cancellationToken = default);
-        Task<UserGoal?> GetUserActiveGoalByTypeAsync(int accountId, GoalTypeEnum goalType, CancellationToken cancellationToken = default);
+        Task<UserGoal?> GetUserActiveGoalByTypeAsync(int userProfileId, GoalTypeEnum goalType, CancellationToken cancellationToken = default);
         Task<bool> IsQuestActiveGoalAsync(int questId, CancellationToken cancellationToken = default);
     }
 }
