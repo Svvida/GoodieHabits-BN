5903c1cd87cfd0c445942f0299bf8d6ceb5484c6
Svvida <116441225+Svvida@users.noreply.github.com>
2025-12-25 23:13:27 +0100

    Prepared endpoint to delete notifications (#321)

---
+++ Application/Notifications/Commands/DeleteNotification/DeleteNotificationCommand.cs
@@ -0,0 +1,7 @@
+﻿using Application.Common.Interfaces;
+using MediatR;
+
+namespace Application.Notifications.Commands.DeleteNotification
+{
+    public record DeleteNotificationCommand(int UserProfileId, Guid NotificationId) : ICommand<Unit>;
+}
